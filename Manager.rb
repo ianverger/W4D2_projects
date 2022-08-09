c46102fd56dd352d592ecc50a1322b3b2d8358df
Chun Lau <dlaucodes@gmail.com>
2022-08-09 14:08:28 -0400

    add manager

---
+++ Manager.rb
@@ -0,0 +1,40 @@
+class Manager < Employee
+
+    attr_reader :employees
+    
+    def initialize
+        @employees = []
+        super
+    end
+
+    def add_employee(employee)
+        @employees << employee
+
+        employee
+    end
+
+    def bonus(mutiplier)
+
+        queue = []
+        total_salaries = 0
+
+        queue << self
+        until queue.empty?
+              queue.each do |employee|
+                first = queue.shift
+                if first.is_a?(Manager)
+                    total_salaries += first.salary if !first.manager.nil?
+                    queue += first.employees
+                else  
+                 total_salaries += first.salary
+                end
+            end
+        end
+        return total_salaries * multiplier
+    end
+    
+    
+end
+ned.bonus(5) # => 500_000
+darren.bonus(4) # => 88_000
+david.bonus(3) # => 30_000
--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -1,4 +1,25 @@
-sdfsdfs
+class Employee 
+    attr_reader :name, :salary, :title
+    attr_accessor :manager
 
-add
-add
+    def initialize(name, salary, manager = nil, title)
+        @name = name
+        @title = title
+        @salary = salary
+        self.manager = manager
+
+    def manager=(manager)
+
+        @manager = manager
+        @manager.add_employee(self) if !@manager.nil?
+
+    end
+
+    #dpes employeed have a manager? 
+
+
+    def bonus(multiplier)
+       return salary * multiplier
+
+    end
+end
